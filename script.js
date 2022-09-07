05b1b14c3a6b0255b5195ce9eb2de13b07424fa3
Abhijith PK <pkabhijith1@gmail.com>
2022-09-07 23:04:36 +0530

    problem with resume

--- script.js
+++ script.js
@@ -2,17 +2,54 @@ textArea=document.querySelector('textarea');
 speechBtn=document.querySelector('button');
 voiceList= document.getElementById('voice');
 
- synth= speechSynthesis;
- 
+ let synth= speechSynthesis;
+ let isSpeaking = true;
+
 speechBtn.addEventListener("click", e=>{
      e.preventDefault();
     if(textArea.value !==""){
         textToSpeech(textArea.value)
     }
+    if(textArea.value.length > 80){
+        if(isSpeaking){
+            synth.resume()
+            isSpeaking=false;
+            speechBtn.innerText = "pause";
+        }else{
+            synth.pause()
+            isSpeaking=true
+            speechBtn.innerText = "Resume";
+        }
+        setInterval(()=>{
+            if(!synth.speaking && isSpeaking){
+                isSpeaking=true;
+                speechBtn.innerText = "start";
+            }
+        });
+    }else{
+        speechBtn.innerText = "Start";
+    }
 });
 
 function textToSpeech(text){
-let speechConvert= new SpeechSynthesisUtterance(text);
-synth.speak(speechConvert);
+let utter= new SpeechSynthesisUtterance(text);
+
+for(let voice of synth.getVoices()){
+    if(voice.name=== voiceList.value){
+        utter.voice=voice;
+    }
 }
 
+synth.speak(utter);
+}
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        console.log(voice);
+        let selected= voice.name ==="Microsoft Anna - English (United States)" ? "selected" : "";
+        let option=`<option value="${voice.name}"${selected}> ${voice.name}(${voice.lang}) </option>`;
+        voiceList.insertAdjacentHTML("beforeend",option);
+    }
+}
+
+synth.addEventListener("voiceschanged",voices)
